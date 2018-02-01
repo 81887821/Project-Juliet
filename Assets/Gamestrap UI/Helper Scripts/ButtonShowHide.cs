719abf05dde3143aa04cc00565c8f9d4d7a70d53
YeonGyu Jeong <81887821@hanmail.net>
2018-02-01 22:34:08 +0900

    Fixed code indentation.

--- Assets/Gamestrap UI/Helper Scripts/ButtonShowHide.cs
+++ Assets/Gamestrap UI/Helper Scripts/ButtonShowHide.cs
@@ -3,31 +3,32 @@ using System.Collections;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
-public class ButtonShowHide : MonoBehaviour {
+public class ButtonShowHide : MonoBehaviour
+{
 
-	public bool startShowGroup;
-	public GameObject[] showHideGroup;
+    public bool startShowGroup;
+    public GameObject[] showHideGroup;
 
-	private bool show;
+    private bool show;
 
-	void Start () {
-		// This automatically registers the event click on the button component
-		GetComponent<Button>().onClick.AddListener(() => { Click(); });
-		show = startShowGroup;
-		ShowHideUpdate();
-	}
+    void Start () {
+        // This automatically registers the event click on the button component
+        GetComponent<Button>().onClick.AddListener(() => { Click(); });
+        show = startShowGroup;
+        ShowHideUpdate();
+    }
 
-	public void Click()
-	{
-		show = !show;
-		ShowHideUpdate();
-	}
+    public void Click()
+    {
+        show = !show;
+        ShowHideUpdate();
+    }
 
-	private void ShowHideUpdate()
-	{
-		foreach (GameObject go in showHideGroup)
-		{
-			go.SetActive(show);
-		}
-	}
+    private void ShowHideUpdate()
+    {
+        foreach (GameObject go in showHideGroup)
+        {
+            go.SetActive(show);
+        }
+    }
 }
--- Assets/Script/StageManager.cs
+++ Assets/Script/StageManager.cs
@@ -6,13 +6,13 @@ public class StageManager : MonoBehaviour
 {
     // Use this for initialization
     void Start ()
-	{
+    {
         
     }
     
     // Update is called once per frame
     void Update ()
-	{
+    {
         
     }
 }
