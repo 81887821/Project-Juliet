1ec049c31fe2a2ea9756e3cd3d51c3033c460d7d
YeonGyu Jeong <81887821@hanmail.net>
2018-02-11 20:27:20 +0900

    Added camera moving script.

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform TraceTarget;
+
+    private void Update()
+    {
+        transform.position = new Vector3(TraceTarget.position.x, TraceTarget.position.y + 3f, -10f);
+    }
+}
