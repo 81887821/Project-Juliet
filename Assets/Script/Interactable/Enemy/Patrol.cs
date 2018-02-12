1d5cce44bc7104e731a27e9be3cc38fc565df501
YeonGyu Jeong <81887821@hanmail.net>
2018-02-12 22:51:04 +0900

    Patrol implemented.

--- Assets/Script/Interactable/Enemy/Patrol.cs
+++ Assets/Script/Interactable/Enemy/Patrol.cs
@@ -1,18 +1,158 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Patrol : Enemy
 {
-    private float attackEnableTimer;
+    private enum PatrolState { NONE, NORMAL, TURNING, ALERT, SEARCHING_LEFT, SEARCHING_RIGHT, HIT, DEAD }
+
+    public float AlertModeSpeedMultiplier = 1.5f;
+
+    private TargetDetector playerDetector;
+    private Animator animator;
+
     private bool attackDisabled;
+
+    private PatrolState state = PatrolState.NORMAL;
+    private PatrolState nextState = PatrolState.NORMAL;
+
+    protected override void Start()
+    {
+        base.Start();
+        playerDetector = GetComponentInChildren<TargetDetector>();
+        animator = GetComponent<Animator>();
+    }
 
     protected override void Update()
     {
         base.Update();
+        UpdateState();
+    }
 
-        if (attackDisabled && Time.time > attackEnableTimer)
-            attackDisabled = false;
+    private void UpdateState()
+    {
+        PatrolState nextStateByEnvironment = GetNextStateByEnvironment();
+        nextState = (nextState > nextStateByEnvironment ? nextState : nextStateByEnvironment);
+
+        if (state != nextState)
+        {
+            HandleStateTransitionSideEffect(state, nextState);
+            state = nextState;
+            UpdateAnimationState(state);
+        }
+
+        nextState = PatrolState.NONE;
+    }
+
+    private PatrolState GetNextStateByEnvironment()
+    {
+        switch (state)
+        {
+            case PatrolState.NORMAL:
+                if (controller.collisions.front || CliffOnFront())
+                    return PatrolState.TURNING;
+                else if (playerDetector.TargetFound)
+                    return PatrolState.ALERT;
+                else
+                    return PatrolState.NORMAL;
+            case PatrolState.TURNING:
+                return PatrolState.NORMAL;
+            case PatrolState.ALERT:
+                if (playerDetector.TargetFound)
+                    return PatrolState.ALERT;
+                else
+                    return PatrolState.SEARCHING_LEFT;
+            case PatrolState.SEARCHING_LEFT:
+                if (playerDetector.TargetFound)
+                    return PatrolState.ALERT;
+                else if (stateEndTime > Time.time)
+                    return PatrolState.SEARCHING_LEFT;
+                else
+                    return PatrolState.SEARCHING_RIGHT;
+            case PatrolState.SEARCHING_RIGHT:
+                if (playerDetector.TargetFound)
+                    return PatrolState.ALERT;
+                else if (stateEndTime > Time.time)
+                    return PatrolState.SEARCHING_RIGHT;
+                else
+                    return PatrolState.NORMAL;
+            case PatrolState.HIT:
+                if (stateEndTime > Time.time)
+                    return PatrolState.HIT;
+                else
+                    return PatrolState.ALERT;
+            case PatrolState.DEAD:
+                return PatrolState.DEAD;
+            default:
+                throw new Exception("Forbidden state for Cleaner : " + state);
+        }
+    }
+
+    private void HandleStateTransitionSideEffect(PatrolState oldState, PatrolState newState)
+    {
+        switch (oldState)
+        {
+            case PatrolState.ALERT:
+            case PatrolState.SEARCHING_LEFT:
+            case PatrolState.SEARCHING_RIGHT:
+                maxSpeed /= AlertModeSpeedMultiplier;
+                break;
+            case PatrolState.HIT:
+                attackDisabled = false;
+                break;
+        }
+
+        switch (newState)
+        {
+            case PatrolState.TURNING:
+                HeadingRight = !HeadingRight;
+                velocity.x = 0f;
+                break;
+            case PatrolState.ALERT:
+                maxSpeed *= AlertModeSpeedMultiplier;
+                break;
+            case PatrolState.SEARCHING_LEFT:
+                maxSpeed *= AlertModeSpeedMultiplier;
+                HeadingRight = false;
+                stateEndTime = Time.time + 1f;
+                break;
+            case PatrolState.SEARCHING_RIGHT:
+                maxSpeed *= AlertModeSpeedMultiplier;
+                HeadingRight = true;
+                stateEndTime = Time.time + 1f;
+                break;
+            case PatrolState.HIT:
+                attackDisabled = true;
+                stateEndTime = Time.time + 0.5f;
+                break;
+            case PatrolState.DEAD:
+                Destroy(gameObject, 1f);
+                break;
+        }
+    }
+
+    private void UpdateAnimationState(PatrolState state)
+    {
+        switch (state)
+        {
+            case PatrolState.NORMAL:
+            case PatrolState.TURNING:
+                animator.Play("PatrolNormal");
+                break;
+            case PatrolState.ALERT:
+            case PatrolState.SEARCHING_LEFT:
+            case PatrolState.SEARCHING_RIGHT:
+            case PatrolState.HIT:
+                animator.Play("PatrolAlert");
+                break;
+            case PatrolState.DEAD:
+                animator.Play("PatrolNormalDead");
+                break;
+            default:
+                throw new Exception("Forbidden state for Patrol : " + state);
+        }
     }
 
     public override void OnAttack(IInteractable target)
@@ -24,20 +164,34 @@ public class Patrol : Enemy
     public override void OnDamaged(IInteractable attacker, int damage, Vector2 knockback)
     {
         base.OnDamaged(attacker, damage, knockback);
-
-        attackDisabled = true;
-        attackEnableTimer = Time.time + 1f;
+        nextState = PatrolState.HIT;
+        UpdateState();
     }
 
     protected override void UpdateVelocity()
     {
-        // TODO : Patrol moving.
-        velocity.x = Mathf.SmoothDamp(velocity.x, 0f, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        float targetVelocityX;
+
+        switch (state)
+        {
+            case PatrolState.NORMAL:
+            case PatrolState.ALERT:
+            case PatrolState.SEARCHING_LEFT:
+            case PatrolState.SEARCHING_RIGHT:
+                targetVelocityX = maxSpeed;
+                break;
+            default:
+                targetVelocityX = 0f;
+                break;
+        }
+
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
         velocity.y += gravity * Time.deltaTime;
     }
 
     public override void Die()
     {
-        Destroy(gameObject);
+        nextState = PatrolState.DEAD;
+        UpdateState();
     }
 }
---
+++ Assets/Script/Interactable/Enemy/TargetDetector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class TargetDetector : MonoBehaviour
+{
+    public LayerMask Targets;
+
+    private int targetCount = 0;
+
+    public bool TargetFound
+    {
+        get
+        {
+            return targetCount > 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Targets.Contains(collision.gameObject.layer))
+            targetCount++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (Targets.Contains(collision.gameObject.layer) && targetCount > 0)
+            targetCount--;
+    }
+}
