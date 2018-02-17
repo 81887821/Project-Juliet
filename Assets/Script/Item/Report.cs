5e0e01ca143f295d282c54652766acc9bf4318b3
YeonGyu Jeong <81887821@hanmail.net>
2018-02-17 19:11:25 +0900

    Added report item.

---
+++ Assets/Script/Item/Report.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Report : Item
+{
+    public override void OnItemGet()
+    {
+        throw new System.NotImplementedException();
+    }
+}
