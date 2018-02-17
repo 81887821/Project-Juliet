675fae6b69c6f9628dddaf16488cdd74cf35078c
YeonGyu Jeong <81887821@hanmail.net>
2018-02-17 20:27:59 +0900

    Kill interactable objects when falled from map.

---
+++ Assets/Script/Interactable/Extra/ImmediateKill.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmediateKill : MonoBehaviour, IInteractable
+{
+    public void Die()
+    {
+        throw new InvalidOperationException("ImmediateKill cannot die.");
+    }
+
+    public void OnAttack(IInteractable target)
+    {
+        target.Die();
+    }
+
+    public void OnDamaged(IInteractable attacker, int damage)
+    {
+        throw new InvalidOperationException("ImmediateKill cannot be damaged.");
+    }
+
+    public void OnDamaged(IInteractable attacker, int damage, Vector2 knockback)
+    {
+        throw new InvalidOperationException("ImmediateKill cannot be damaged.");
+    }
+}
