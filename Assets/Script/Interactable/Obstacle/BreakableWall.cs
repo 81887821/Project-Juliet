2ecb8aaf051c4bc65150c4c0ea20003789ae013e
YeonGyu Jeong <81887821@hanmail.net>
2018-02-17 18:02:28 +0900

    Added breakable wall, add obstacles to Player's target mask.

---
+++ Assets/Script/Interactable/Obstacle/BreakableWall.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableWall : MonoBehaviour, IInteractable
+{
+    public int CurrentHealth = 1;
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnAttack(IInteractable target)
+    {
+        throw new InvalidOperationException("Wall cannot attack.");
+    }
+
+    public void OnDamaged(IInteractable attacker, int damage)
+    {
+        CurrentHealth -= damage;
+
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    public void OnDamaged(IInteractable attacker, int damage, Vector2 knockback)
+    {
+        // Wall ignores knockback.
+        OnDamaged(attacker, damage);
+    }
+}
