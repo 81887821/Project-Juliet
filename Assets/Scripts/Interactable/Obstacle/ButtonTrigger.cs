19fa4fd4b746d2310dbd32656fa8dc8ce5e25052
YeonGyu Jeong <81887821@hanmail.net>
2018-02-22 13:28:55 +0900

    Added slide door and button.

---
+++ Assets/Scripts/Interactable/Obstacle/ButtonTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ButtonTrigger : MonoBehaviour
+{
+    public LayerMask InteractableMask;
+    public UnityEvent OnButtonPressed;
+    public UnityEvent OnButtonReleased;
+
+    private bool Pressing
+    {
+        get
+        {
+            return pressing;
+        }
+        set
+        {
+            if (pressing != value)
+            {
+                pressing = value;
+                if (value == true)
+                    OnButtonPressed.Invoke();
+                else
+                    OnButtonReleased.Invoke();
+            }
+        }
+    }
+
+    private BoxCollider2D boxCollider;
+    private bool pressing = false;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (Physics2D.OverlapBox(boxCollider.bounds.center, boxCollider.bounds.size, 0f, InteractableMask))
+            Pressing = true;
+        else
+            Pressing = false;
+    }
+}
---
+++ Assets/Scripts/Interactable/Obstacle/SlideDoor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SlideDoor : MonoBehaviour
+{
+    public Vector2 LocalOpenPosition;
+    public float Duration;
+
+    private Vector3 globalClosedPosition;
+    private Vector3 globalOpenPosition;
+
+    private void Awake()
+    {
+        globalClosedPosition = transform.position;
+        globalOpenPosition = transform.position + (Vector3)LocalOpenPosition;
+    }
+
+    public void Open()
+    {
+        StopAllCoroutines();
+        StartCoroutine(MoveDoor(globalClosedPosition, globalOpenPosition));
+    }
+    
+    public void Close()
+    {
+        StopAllCoroutines();
+        StartCoroutine(MoveDoor(globalOpenPosition, globalClosedPosition));
+    }
+
+    private IEnumerator MoveDoor(Vector3 startPosition, Vector3 endPosition)
+    {
+        Vector3 totalMovement = endPosition - startPosition;
+        float progress = (transform.position - startPosition).sqrMagnitude / totalMovement.sqrMagnitude;
+
+        while (progress < 1f)
+        {
+            transform.position = startPosition + totalMovement * progress;
+            yield return new WaitForEndOfFrame();
+            progress += Time.deltaTime / Duration;
+        }
+
+        transform.position = endPosition;
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying)
+        {
+            Gizmos.color = Color.blue;
+            float size = .3f;
+            Vector3 globalOpenPosition = (Vector3)LocalOpenPosition + transform.position;
+
+            Gizmos.DrawLine(globalOpenPosition - Vector3.up * size, globalOpenPosition + Vector3.up * size);
+            Gizmos.DrawLine(globalOpenPosition - Vector3.left * size, globalOpenPosition + Vector3.left * size);
+            Handles.Label(globalOpenPosition, "Opened");
+        }
+    }
+#endif
+}
