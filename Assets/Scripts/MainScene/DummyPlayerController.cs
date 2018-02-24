775ba27e2fefb7562ef4d84d549be3e68773f35a
YeonGyu Jeong <81887821@hanmail.net>
2018-02-24 17:47:23 +0900

    Added Main scene.

---
+++ Assets/Scripts/MainScene/DummyPlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DummyPlayerController : MonoBehaviour {
+
+    public static DummyPlayerController Instance
+    {
+        get;
+        private set;
+    }
+
+    public float Speed = 18f;
+
+    public Transform TargetLocation
+    {
+        get
+        {
+            return moveTarget;
+        }
+
+        set
+        {
+            moveTarget = value;
+            if (movingCoroutine != null)
+                StopCoroutine(movingCoroutine);
+            movingCoroutine = StartCoroutine(Move(moveTarget));
+        }
+    }
+
+    private Animator animator;
+    private Transform moveTarget;
+    private Coroutine movingCoroutine;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Double instantiation : " + this);
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+        animator = GetComponent<Animator>();
+    }
+
+    private IEnumerator Move(Transform target)
+    {
+        float displacement = target.position.x - transform.position.x;
+
+        animator.Play("JuliaWalk");
+
+        if (displacement < 0)
+        {
+            transform.rotation = new Quaternion(0f, 1f, 0f, 0f);
+            while (displacement < 0)
+            {
+                transform.Translate(-Speed * Time.deltaTime, 0f, 0f, Space.World);
+                yield return new WaitForEndOfFrame();
+                displacement = target.position.x - transform.position.x;
+            }
+        }
+        else
+        {
+            transform.rotation = new Quaternion(0f, 0f, 0f, 1f);
+            while (displacement > 0)
+            {
+                transform.Translate(Speed * Time.deltaTime, 0f, 0f, Space.World);
+                yield return new WaitForEndOfFrame();
+                displacement = target.position.x - transform.position.x;
+            }
+        }
+
+        animator.Play("JuliaIdle");
+        movingCoroutine = null;
+    }
+}
---
+++ Assets/Scripts/MainScene/StageEntry.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class StageEntry : MonoBehaviour
+{
+    public string SceneName;
+    public Sprite SpriteOnStageAvailable;
+    public Sprite SpriteOnStageUnavailable;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        var player = DummyPlayerController.Instance;
+
+        if (player.TargetLocation != transform)
+            player.TargetLocation = transform;
+        else
+            SceneManager.LoadScene("Scenes/" + SceneName);
+    }
+}
