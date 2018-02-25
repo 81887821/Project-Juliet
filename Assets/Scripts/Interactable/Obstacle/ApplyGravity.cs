628126ac6145594637edb8624bd6c974709cb087
YeonGyu Jeong <81887821@hanmail.net>
2018-02-25 11:15:45 +0900

    More sophisticated collision check for gravity wall.

--- Assets/Scripts/Interactable/Obstacle/ApplyGravity.cs
+++ Assets/Scripts/Interactable/Obstacle/ApplyGravity.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class ApplyGravity : MonoBehaviour
 {
+    private const int RAY_COUNT = 3;
+
     public LayerMask CollisionMask;
     public float Gravity = -150f;
 
     private float speed = 0f;
     private Vector2 localRaycastOrigin;
+    private float distanceBetweenRays;
     private BoxCollider2D boxCollider;
 
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider2D>();
-        localRaycastOrigin = new Vector2(boxCollider.offset.x, boxCollider.offset.y - boxCollider.size.y / 2f);
+        localRaycastOrigin = new Vector2(boxCollider.offset.x - boxCollider.size.x / 2f, boxCollider.offset.y - boxCollider.size.y / 2f);
+        distanceBetweenRays = boxCollider.size.x / 2f;
 #if DEBUG
         if (Gravity > 0)
             Debug.LogErrorFormat("[{0}] To collision check work properly, Gravity must be negative.", name);
@@ -27,10 +31,6 @@ public class ApplyGravity : MonoBehaviour
         float moveAmount = speed * Time.deltaTime;
         bool collided = CollisionCheck(ref moveAmount);
 
-#if DEBUG
-        Debug.DrawRay(GetRaycastOrigin(), Vector2.up * moveAmount, collided ? Color.red : Color.white);
-#endif
-
         if (collided)
             speed = 0f;
         else
@@ -40,23 +40,30 @@ public class ApplyGravity : MonoBehaviour
         }
     }
 
-    private Vector2 GetRaycastOrigin()
+    private Vector2 GetRaycastOrigin(int rayNumber)
     {
-        return localRaycastOrigin.ToGlobalPosition(this);
+        return (localRaycastOrigin + new Vector2(distanceBetweenRays * rayNumber, 0f)).ToGlobalPosition(this);
     }
 
     private bool CollisionCheck(ref float moveAmount)
     {
         float absoluteMoveAmount = Mathf.Abs(moveAmount);
 
-        foreach (RaycastHit2D hit in Physics2D.RaycastAll(GetRaycastOrigin(), Vector2.down, absoluteMoveAmount, CollisionMask))
+        for (int i = 0; i < RAY_COUNT; i++)
         {
-            if (hit.transform == transform) // To prevent self collision.
-                continue;
-            else if (hit.distance == 0f)
-                return true;
-            else if (hit.distance < absoluteMoveAmount)
-                absoluteMoveAmount = hit.distance;
+            RaycastHit2D[] hits = Physics2D.RaycastAll(GetRaycastOrigin(i), Vector2.down, absoluteMoveAmount, CollisionMask);
+#if DEBUG
+            Debug.DrawRay(GetRaycastOrigin(i), Vector2.down * absoluteMoveAmount, hits.Length != 0 ? Color.red : Color.white);
+#endif
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.transform == transform) // To prevent self collision.
+                    continue;
+                else if (hit.distance == 0f)
+                    return true;
+                else if (hit.distance < absoluteMoveAmount)
+                    absoluteMoveAmount = hit.distance;
+            }
         }
 
         moveAmount = Mathf.Sign(moveAmount) * absoluteMoveAmount;
